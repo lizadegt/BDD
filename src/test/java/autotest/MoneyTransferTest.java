14bc8563467993e63e6137973351d4456825868c
elizavetadegtareva <liza.degt@yandex.ru>
2021-12-28 14:41:23 +0300

    additional tests

--- src/test/java/autotest/MoneyTransferTest.java
+++ src/test/java/autotest/MoneyTransferTest.java
@@ -29,5 +29,45 @@ public class MoneyTransferTest {
         assertEquals(expectedBalanceOneCard, actualBalanceOneCard);
         assertEquals(expectedBalanceTwoCard, actualBalanceTwoCard);
     }
+
+    @Test
+    void shouldTransferWithTwoOnOne() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
+        var firstCardInfo = getOneCardNum();
+        var secondCardInfo = getTwoCardNum();
+        int amount = 5000;
+        var expectedBalanceOneCard = dashboardPage.getOneCardBalance() + amount;
+        var expectedBalanceTwoCard = dashboardPage.getTwoCardBalance() - amount;
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        dashboardPage = transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+        var actualBalanceOneCard = dashboardPage.getOneCardBalance();
+        var actualBalanceTwoCard = dashboardPage.getTwoCardBalance();
+        assertEquals(expectedBalanceOneCard, actualBalanceOneCard);
+        assertEquals(expectedBalanceTwoCard, actualBalanceTwoCard);
+    }
+
+    @Test
+    void shouldTransferMoreBalance() {
+            var loginPage = open("http://localhost:9999", LoginPage.class);
+            var authInfo = DataHelper.getAuthInfo();
+            var verificationPage = loginPage.validLogin(authInfo);
+            var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+            var dashboardPage = verificationPage.validVerify(verificationCode);
+            var firstCardInfo = getOneCardNum();
+            var secondCardInfo = getTwoCardNum();
+            int amount = 200000;
+            var expectedBalanceOneCard = dashboardPage.getOneCardBalance() - amount;
+            var expectedBalanceTwoCard = dashboardPage.getTwoCardBalance() + amount;
+            var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+            dashboardPage = transferPage.makeTransfer(String.valueOf(amount), firstCardInfo);
+            var actualBalanceOneCard = dashboardPage.getOneCardBalance();
+            var actualBalanceTwoCard = dashboardPage.getTwoCardBalance();
+            assertEquals(expectedBalanceOneCard, actualBalanceOneCard);
+            assertEquals(expectedBalanceTwoCard, actualBalanceTwoCard);
+    }
 }
 
